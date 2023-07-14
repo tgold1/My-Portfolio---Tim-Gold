d4fbb89560483cf23569c8f1cc8bcdec53f8aa79
Tim Gold <timothygold@comcast.net>
2023-07-14 15:56:52 -0400

    made changes to data.js in testimonials const and changes to the Testimonial function in testimonials.js

--- my-portfolio/src/components/Testimonials.js
+++ my-portfolio/src/components/Testimonials.js
@@ -2,9 +2,9 @@ import React from "react";
 import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
 import { testimonials } from "../data";
 
-export default function Testimonials () {
-    return (
-        <section id="testimonials">
+export default function Testimonials() {
+  return (
+    <section id="testimonials">
       <div className="container px-5 py-10 mx-auto text-center">
         <UsersIcon className="w-10 inline-block mb-4" />
         <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
@@ -12,30 +12,30 @@ export default function Testimonials () {
         </h1>
         <div className="flex flex-wrap m-4">
           {testimonials.map((testimonial) => (
-            <div className="p-4 md:w-1/2 w-full">
-              <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
-                <TerminalIcon className="block w-8 text-gray-500 mb-4" />
-                <p className="leading-relaxed mb-6">{testimonial.quote}</p>
-                <div className="inline-flex items-center">
-                  <img
-                    alt="testimonial"
-                    src={testimonial.image}
-                    className="w-12 rounded-full flex-shrink-0 object-cover object-center"
-                  />
-                  <span className="flex-grow flex flex-col pl-4">
-                    <span className="title-font font-medium text-white">
-                      {testimonial.name}
-                    </span>
-                    <span className="text-gray-500 text-sm uppercase">
-                      {testimonial.company}
-                    </span>
-                  </span>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section> 
-    );
+             <a
+             href={testimonial.link}  
+             key={testimonial.quote}
+             className="sm:w-1/2 w-100 p-4">
+             <div className="flex relative">
+               <img
+                 alt="gallery"
+                 className="absolute inset-0 w-full h-full object-cover object-center"
+                 src={testimonial.image}
+               />
+               <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                 <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                   {testimonial.subtitle}
+                 </h2>
+                 <h1 className="title-font text-lg font-medium text-white mb-3">
+                   {testimonial.title}
+                 </h1>
+                 <p className="leading-relaxed">{testimonial.description}</p>
+               </div>
+             </div>
+           </a>
+         ))}
+       </div>
+     </div>
+    </section>
+  );
 }
--- my-portfolio/src/data.js
+++ my-portfolio/src/data.js
@@ -52,15 +52,15 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
+      "Download My Resume Here",
+    link: "https://1drv.ms/b/s!AnzGKXC9XqbymD0NNReje98yDngW?e=OfWK1h",
     name: "Jesse Hicks",
     company: "Zoozle",
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    "My LinkedIn Profile",
+    link: "https://www.linkedin.com/in/tim-gold-81a9164a",
     name: "Ruben Alvarez",
     company: "Dooble.io",
   },
